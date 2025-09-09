96548f3aa86c0939659e7abc688612db30b10ff5
Taurat-kibria <tauratkibria9@gmail.com>
2025-09-09 23:10:19 +0600

    active elements done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
+function manageSpinner(status){
+    if(status === true){
+        document.getElementById("spinner_id").classList.remove("hidden");
+        document.getElementById("card_container_id").classList.add("hidden");
+    }
+    else{
+        document.getElementById("spinner_id").classList.add("hidden");
+        document.getElementById("card_container_id").classList.remove("hidden");
+    }
+}
+
+
 const loadCatagories = () => {
+    
     fetch("https://openapi.programming-hero.com/api/categories")
         .then(res => res.json())
         .then(data => displayCatagories(data.categories))
@@ -10,32 +23,51 @@ const loadCatagories = () => {
 function displayCatagories(cats) {
     const catContainer = document.getElementById("cat_container")
     cats.forEach(element => {
+        
+         
 
         const newCat = document.createElement("div");
         newCat.innerHTML = `
-         <div  onclick = "loadCard(${element.id})"
-         class = "text-[#57696c] text-xl
-         ">${element.category_name}</div>  
+         <div id="active_${element.id}"  onclick = "loadCard(${element.id})"
+         class = "active text-[#57696c] text-xl py-2 pl-2
+          hover:bg-[#15803d] hover:text-white
+           rounded-sm">${element.category_name}</div>  
         `
         catContainer.appendChild(newCat);
+
+        
+        
     });
+   
 
 }
 
 function loadCard(id) {
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayCard(data.plants));
+        .then(data => {
+            displayCard(data.plants)
+             giveColor(id)
+       
+       
+            
+        });
 }
 
 
 
 function loadCart(id) {
     fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
-    .then(res => res.json())
-    .then(data => displayCart(data.plants));
+        .then(res => res.json())
+        .then(data => displayCart(data.plants));
 }
+
+function remove_cart(plants){
+    console.log(plants)
+        document.getElementById(`cross_${plants}`).innerHTML = "";
+    }
 
 function displayCart(plants) {
 
@@ -44,8 +76,8 @@ function displayCart(plants) {
     const notunCart = document.createElement("div");
     notunCart.innerHTML =
         `
-        <div class="flex justify-between items-center
-                 bg-[#f0fdf4] py-3 px-2 rounded-lg">
+        <div id = "cross_${plants.id}" class="flex justify-between items-center
+                 bg-[#f0fdf4] py-3 px-2 rounded-lg mb-3">
                     <div>
                         <h1 class="font-semibold mb-1">${plants.name}</h1>
                         <p class="text-[#57696c]">
@@ -53,14 +85,25 @@ function displayCart(plants) {
                         </p>
                     </div>
                     <div>
-                        <button class="text-[#57696c] ">
+                        <button onclick = "remove_cart(${plants.id})" class="text-[#57696c] ">
                             <i class="fa-solid fa-xmark"></i>
                         </button>
                     </div>
                 </div>
         `
     cartContainer.appendChild(notunCart);
+
+    const plantPrice = parseInt(plants.price);
+    let precPrice = parseInt(document.getElementById("total_id").innerText);
+    precPrice = plantPrice + precPrice;
+    document.getElementById("total_id").innerText = precPrice;
+
+    
+
+
+
 }
+
 
 function displayCard(plants) {
     const cardContainer = document.getElementById("card_container_id");
@@ -68,8 +111,8 @@ function displayCard(plants) {
     plants.forEach(element => {
         const notunEl = document.createElement("div");
         notunEl.innerHTML = `
-         <div class="">
-                    <div class=" bg-white h-full rounded-lg space-y-4 p-4">
+         <div  class="">
+                    <div  class=" bg-white h-full rounded-lg space-y-4 p-4">
                         <img class=" w-full  max-h-[200px]  
                          rounded-md" src="${element.image}" alt="no img">
                         <p onclick = "loadModal(${element.id})" class="font-semibold text-xl ">${element.name}</p>
@@ -107,15 +150,18 @@ function displayCard(plants) {
          `
 
         cardContainer.appendChild(notunEl);
+        
+        
     });
+    manageSpinner(false);
 }
 
 
 // modal funtion
-function loadModal(id){
+function loadModal(id) {
     fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
-    .then(res => res.json())
-    .then(data => displaModal(data.plants))
+        .then(res => res.json())
+        .then(data => displaModal(data.plants))
 }
 
 // lants": {
@@ -126,19 +172,30 @@ function loadModal(id){
 // "category": "Climber",
 // "price": 350
 
-function displaModal(plants){
+function displaModal(plants) {
 
     const modal = document.getElementById("modal_comp");
     modal.innerHTML = "";
     const div = document.createElement("div");
-    div.innerHTML = 
+    div.innerHTML =
         `
         <h1 class = "text-2xl mb-5 font-bold">${plants.name}</h1>
 
         <p class = "font-semibold">${plants.description}</p>
         `
-        modal.appendChild(div)
-        document.getElementById("my_modal_5").showModal();
+    modal.appendChild(div)
+    document.getElementById("my_modal_5").showModal();
+}
+
+function giveColor(id){
+    const catagors = document.querySelectorAll(".active");
+    catagors.forEach(element => {
+        
+        element.classList.remove("stay_color")
+    });
+    
+    document.getElementById(`active_${id}`).classList.add("stay_color")
+    
 }
 
 loadCatagories();
